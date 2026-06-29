c54e6a5a852be03c896018270c09dbaa68e047a0
vidovics <16019608+vidovics@users.noreply.github.com>
2026-06-29 18:28:36 +0100

    Add files via upload

---
+++ _common.R
@@ -0,0 +1,17 @@
+# _common.R
+
+#
+install.packages("tidyverse","readr","igraph","tidygraph","ggraph","ggrepel","visNetwork")
+
+# Core data wrangling
+library(tidyverse)
+library(readr)
+
+# Network analysis
+library(igraph)
+library(tidygraph)
+library(ggraph)
+
+# Visualisation
+library(ggrepel)
+library(visNetwork)
